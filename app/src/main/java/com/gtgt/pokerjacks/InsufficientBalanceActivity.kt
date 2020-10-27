891377f807250f0f773372332e7951538040b46f
gorleyogi@gmail.com <GY@98vm@>
2020-10-27 17:40:21 +0530

    Done Insufficient Balance design and added json files for debug and production

---
+++ app/src/main/java/com/gtgt/pokerjacks/InsufficientBalanceActivity.kt
@@ -0,0 +1,18 @@
+package com.gtgt.pokerjacks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar_layout.*
+
+class InsufficientBalanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insufficient_balance)
+
+        supportActionBar?.hide()
+
+        tv_commonTitle.text = "Insufficient Balance"
+
+
+    }
+}
